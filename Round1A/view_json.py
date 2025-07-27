9fefcafb59da91b89a313f424a2b582882300970
Deepthi <deepthipriyanka004@gmail.com>
2025-07-27 16:47:01 +0530

    Adobe Hackathon 2025

--- Round1A/view_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_path = "app/output/Untitled document (2).json"
-
-with open(json_path, encoding="utf-8") as f:
-    data = json.load(f)
-
-print(json.dumps(data, indent=2, ensure_ascii=False))
